ecff970cdec18710bd6bdc546640e78b94d44b06
gautamvaishnav-git <gauravvaishnav8690@gmail.com>
2023-11-17 16:24:49 +0530

    minor fix

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Hero() {
   return (
     <section className="flex items-center justify-between pt-7">
       <div className="md:block md:w-full hidden">
         <h2 className="text-9xl font-extrabold tracking-wide">FOR NEXT</h2>
-        <h2 className="text-7xl font-extrabold pt-3">{"Let's"} Grow together !</h2>
+        <h2 className="text-7xl font-extrabold pt-3">
+          {"Let's"} Grow together !
+        </h2>
       </div>
       <div className="md:max-w-full max-w-xl mx-auto">
         <h1 className="md:text-5xl text-2xl sm:text-4xl font-bold md:text-left text-center">
@@ -20,8 +23,11 @@ export default function Hero() {
           quibusdam quia quae quod voluptates laborum voluptate nemo sint.
         </p>
         <div className="flex gap-2 md:justify-normal justify-center md:pt-0 pt-6">
-          <Button>Work with us!</Button>
-          <Button variant={"outline"}>Join us!</Button>
+          <Button>
+            <Link href="https://www.freelancer.in/u/PradhumnSingh" target="_blank">
+              Work with us!
+            </Link>
+          </Button>
         </div>
       </div>
     </section>
